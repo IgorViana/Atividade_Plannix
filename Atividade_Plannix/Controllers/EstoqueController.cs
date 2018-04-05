bfe88b987cd855f6b7d2578c0445d04818a48b63
Igor Plannix <igor@plannix.com>
2018-04-05 16:34:20 -0300

    Começo atividade Plannix

---
+++ Atividade_Plannix/Controllers/EstoqueController.cs
@@ -0,0 +1,49 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Atividade_Plannix.Controllers
+{
+    public class EstoqueController : ApiController
+    {
+       public  Tijolo[] tijolos = new Tijolo[10];
+       
+        [HttpGet]
+        public Tijolo GetTijoloPorSerial(int serialDigitado)
+        {
+            Tijolo tijoloPorSerial = tijolos.FirstOrDefault((t) => t.serial == this.serialDigitado);
+            return tijoloPorSerial;
+        }
+
+
+        [HttpPost]
+        public List<Tijolo> AddTijolo(Tijolo tijolo)
+        {
+
+            int index = tijolos.Length;
+            tijolos[index] = tijolo;
+
+            return tijolos;
+        }
+
+        public List<Tijolo> RetornarTijolos()
+        {
+            Tijolo temp;
+            for (int i = 0; i < tijolos.Length; i++)
+            {
+                temp = tijolos[i];
+                
+
+            }
+            foreach (Tijolo a in tijolos)
+            {
+                return a;
+            }
+        }
+    }
+}
---
+++ Models/Tijolo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Tijolo
+    {
+        public Tijolo(){ }
+        public Tijolo(string Matrial, DateTime Date, string Fabrica, int Serial)
+        {
+            material = Matrial;
+            date = Date;
+            fabrica = Fabrica;
+            serial = Serial;
+        }
+
+
+        public string material { get; set; }
+        public DateTime date { get; set; }
+        public string fabrica { get; set; }
+        public int serial { get; set; }
+
+    }
+}
