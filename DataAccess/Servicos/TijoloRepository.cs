355f00cfaceef3ffc26466a81dc907f52fad646f
Igor Plannix <igor@plannix.com>
2018-04-06 16:31:17 -0300

    Começo do sistema de ordenação

--- DataAccess/Servicos/TijoloRepository.cs
+++ DataAccess/Servicos/TijoloRepository.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Servicos
 {
     public class TijoloRepository : ITijoloRepository
     {
-        
+
         Tijolo[] arrayDeTijolos = new Tijolo[10];
 
 
@@ -62,5 +62,50 @@ namespace DataAccess.Servicos
             }
             return arrayDeTijolos;
         }
+
+        public Array OrdenarCresOuDecr(string condicao)
+        {
+            Tijolo repos;
+            Tijolo[] arrayDeTijolosOrdenado = arrayDeTijolos;
+            if (condicao.Equals("Cres"))
+            {
+                //percorrer array
+                for (int i = 0; i < arrayDeTijolosOrdenado.Length - 1; i++)
+                {
+                    for (int j = 0; j < arrayDeTijolosOrdenado.Length - (i + 1); j++)
+                    {
+                        //Confere ano
+                        if (arrayDeTijolosOrdenado[j].serial < arrayDeTijolosOrdenado[j + 1].serial)
+                        {
+                            repos = arrayDeTijolosOrdenado[j];
+                            arrayDeTijolosOrdenado[j] = arrayDeTijolosOrdenado[j + 1];
+                            arrayDeTijolosOrdenado[j + 1] = repos;
+                        }
+                    }
+                }
+                return arrayDeTijolosOrdenado;
+            }
+            else if (condicao.Equals("Decr"))
+            {
+                //percorrer array
+                for (int i = 0; i < arrayDeTijolosOrdenado.Length - 1; i++)
+                {
+                    for (int j = 0; j < arrayDeTijolosOrdenado.Length - (i + 1); j++)
+                    {
+                        //Confere ano
+                        if (arrayDeTijolosOrdenado[j].serial < arrayDeTijolosOrdenado[j + 1].serial)
+                        {
+                            repos = arrayDeTijolosOrdenado[j];
+                            arrayDeTijolosOrdenado[j] = arrayDeTijolosOrdenado[j + 1];
+                            arrayDeTijolosOrdenado[j + 1] = repos;
+                        }
+                    }
+                }
+                return arrayDeTijolosOrdenado;
+            }
+
+
+        }
+
     }
 }
